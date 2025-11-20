35befb635b83805ce533fa96203d8801a9e86776
habiba hussein <habiba.hussein@moringaschool.comclear>
2025-11-21 01:08:17 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+# your code here
+def find_anagrams(word, candidates):
+    sorted_word = sorted(word.lower())
+    return [candidate for candidate in candidates if sorted(candidate.lower()) == sorted_word and candidate.lower() != word.lower()]
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, candidates):
+        return find_anagrams(self.word, candidates)
